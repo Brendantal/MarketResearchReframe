f29575216357d3a2157ccb610ccff08cde861326
Brendantal <137125508+Brendantal@users.noreply.github.com>
2024-09-24 11:11:55 -0700

    Add layers.js via QGIS2web export

---
+++ qgis2web/layers.js
@@ -0,0 +1,33 @@
+var wms_layers = [];
+
+var group_MarketSizing = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "Market Sizing"});
+var group_ZoningReform = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "Zoning Reform"});
+var group_IECCAdoption = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "IECC Adoption"});
+var group_MountainTowns = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "Mountain Towns"});
+var group_ManufacturingBoomtownAnalysis = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "Manufacturing Boomtown Analysis"});
+var group_AreaMedianIncome = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "Area Median Income"});
+var group_UnderlyingFiles = new ol.layer.Group({
+                                layers: [],
+                                fold: "open",
+                                title: "Underlying Files"});
+
+
+var layersList = [];
